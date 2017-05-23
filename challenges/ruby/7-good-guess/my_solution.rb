8d4db42b8ee5d77d37f213973f27f993a4440afa
Miguel Soler <msoler735@gmail.com>
2017-05-22 21:00:48 -0500

    Resolved challenge number seven

--- challenges/ruby/7-good-guess/my_solution.rb
+++ challenges/ruby/7-good-guess/my_solution.rb
@@ -1,11 +1,10 @@
 # Good Guess
 
 # Tu solucion abajo:
-
-def good_guess?(a)
-  if a == 42
-    true
-  else
-    false
-  end
+def good_guess?(num)
+	if num == 42
+		true
+	else 
+		false
+	end	
 end
